586c972beda19f81544b7de268da07d5d5182067
Jiaxiang Wu <jiaxiang.wu.90@gmail.com>
2016-01-27 19:58:41 +0800

    create the main program entry

---
+++ Main.cc
@@ -0,0 +1,15 @@
+/*
+  File Name: Main.cc
+  Author: Jiaxiang Wu
+  E-mail: jiaxiang.wu.90@gmail.com
+  Created Time: Wed 27 Jan 2016 07:55:01 PM CST
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "Hello, world!" << std::endl;
+
+  return 0;
+}
+
